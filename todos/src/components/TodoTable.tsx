f7dfc22a25d28519d8fd7080b870ab64a63f8b35
arpitapanda17 <arp2908@gmail.com>
2024-08-20 22:57:48 +0530

    conversion to Typescript project changes

---
+++ todos/src/components/TodoTable.tsx
@@ -0,0 +1,29 @@
+import { TodoModel } from "../models/TodoModel";
+import TodoRowItem from "./TodoRowItem";
+function TodoTable(props: {todos: TodoModel[],
+                            deleteTodo: Function
+}){
+    return(
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Assigned</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.todos.map(todo =>(
+                    <TodoRowItem deleteTodo={props.deleteTodo}
+                        key={todo.rowNumber}
+                        rowNumber = {todo.rowNumber}
+                        rowDescription ={todo.rowDescription}
+                        rowAssigned ={todo.rowAssigned}
+                    />
+                    
+                ))}
+            </tbody>
+        </table>
+    )
+}
+export default TodoTable;
---
+++ todos/src/models/TodoModel.ts
@@ -0,0 +1,10 @@
+export class TodoModel{
+    rowNumber:number;
+    rowDescription:string;
+    rowAssigned:string;
+    constructor(rowNumber:number,rowDescription:string,rowAssigned:string){
+        this.rowNumber=rowNumber;
+        this.rowDescription=rowDescription;
+        this.rowAssigned=rowAssigned;
+    }
+}
